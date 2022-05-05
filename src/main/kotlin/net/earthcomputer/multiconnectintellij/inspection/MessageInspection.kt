da03075ac17186093dd475254c2514d9186d5fa5
Joe <burtonjae@hotmail.co.uk>
2022-05-05 21:33:39 +0100

    Add @Message inspection

---
+++ src/main/kotlin/net/earthcomputer/multiconnectintellij/inspection/MessageInspection.kt
@@ -0,0 +1,50 @@
+package net.earthcomputer.multiconnectintellij.inspection
+
+import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.psi.PsiAnonymousClass
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiModifier
+import com.intellij.psi.search.searches.ClassInheritorsSearch
+import net.earthcomputer.multiconnectintellij.Constants
+
+class MessageInspection : AbstractBaseJavaLocalInspectionTool() {
+    override fun getStaticDescription() = "Reports problems with @Message interfaces"
+
+    override fun checkClass(
+        clazz: PsiClass,
+        manager: InspectionManager,
+        isOnTheFly: Boolean
+    ): Array<ProblemDescriptor>? {
+        if (!clazz.hasAnnotation(Constants.MESSAGE)) {
+            return null
+        }
+
+        if (!clazz.isInterface || clazz.isAnnotationType) {
+            return manager.createProblem(
+                (clazz as? PsiAnonymousClass)?.baseClassReference ?: clazz.nameIdentifier ?: return null,
+                isOnTheFly,
+                "@Message classes must be interfaces"
+            )
+        }
+
+        if (!clazz.hasModifierProperty(PsiModifier.PUBLIC)) {
+            return manager.createProblem(
+                clazz.nameIdentifier ?: return null,
+                isOnTheFly,
+                "@Message must be public"
+            )
+        }
+
+        if (!ClassInheritorsSearch.search(clazz).anyMatch { it.hasAnnotation(Constants.MESSAGE_VARIANT) }) {
+            return manager.createProblem(
+                clazz.nameIdentifier ?: return null,
+                isOnTheFly,
+                "@Message has no variants"
+            )
+        }
+
+        return null
+    }
+}
